ec36beb6f1144530c5be6d830ed9fadafc25d951
Arkadiusz Więcław <arekwie1998@onet.pl>
2024-10-09 15:32:54 +0200

    Adding files

---
+++ Records/Program.cs
@@ -0,0 +1,62 @@
+﻿
+using System.Text;
+
+var currency1 = new CurrencyRecord("USD", 100, "SomeValue");
+var currency2 = currency1 with { Value = 200 }; // cloning of records
+
+var (name, value, _) = currency1; // deconstruction of the record
+
+Console.WriteLine(currency1);
+Console.WriteLine(currency2);
+Console.WriteLine($"Is currency1 == currency2 ? {currency1 == currency2}");
+Console.WriteLine($"Is object.ReferenceEquals(currency1, currency2) ? {object.ReferenceEquals(currency1, currency2)}");
+
+public abstract record BaseCurrencyRecort(string SomeValue);
+// record automatically overrides the methods ToString(), Equals(), GetHashCode() operators == and !=
+// record can automatically create constructor
+public record class CurrencyRecord(string Name, decimal Value, string SomeValue) : BaseCurrencyRecort(SomeValue), IDisposable
+{
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class CurrencyClass
+{
+    public CurrencyClass( string name, decimal value)
+    {
+        Name = name;
+        Value = value;
+    }
+    public string Name { get; set; }
+    public decimal Value { get; set; }
+
+    public override string ToString()
+    {
+        StringBuilder builder = new StringBuilder();
+
+        builder.Append($"Currency: {{Name = {Name}, Value = {Value}}}");
+
+        return builder.ToString();
+    }
+
+    public static bool operator !=(CurrencyClass left, CurrencyClass right)
+    {
+        return !(left == right);
+    }
+
+    public static bool operator ==(CurrencyClass left, CurrencyClass right)
+    {
+        return (object)left == right || ((object) left != null && left.Equals(right));
+    }
+
+    public override int GetHashCode()
+    {
+        return Value.GetHashCode() ^ Name.GetHashCode();
+    }
+
+
+}
+
+
--- ReflectionAndAttributes/obj/Debug/net8.0/ReflectionAndAttributes.AssemblyInfo.cs
+++ ReflectionAndAttributes/obj/Debug/net8.0/ReflectionAndAttributes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReflectionAndAttributes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38d46a118892815f1514a3378c7194ba7b9bd1c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b7bbd8c5c0c623d44966adf16b6b72c850c1188")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReflectionAndAttributes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReflectionAndAttributes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
